16d99d3640cc392e3398a7a505bf08ab15d209f9
adse1823 <aditya.sb2000@gmail.com>
2024-02-25 11:13:12 -0500

    info label

---
+++ src/main/java/Vitorosa/Info.java
@@ -0,0 +1,15 @@
+package Vitorosa;
+
+public class Info {
+    public static String text = "You are the mayor of the town of Vitorosa, a small south American town.\r\n" + //
+                "Your town’s major export is flour made from a special kind of wheat called Miracle wheat.\r\n" + //
+                "It is processed into flour and exported out of the town every month.\r\n" + //
+                "Your wheat needs water from a distant lake. For that you need to generate electricity.\r\n" + //
+                "You need to buy coal for the power plant. You use too much coal - the air pollution spoils the crops.\r\n" + //
+                "You can use solar energy as an alternative, but it is not efficient enough yet.\r\n" + //
+                "You use too much natural energy, the net quantity of wheat harvested drops.\r\n" + //
+                "Your task is to get as much wheat produce at the end of the year as possible.  ";
+
+    
+
+}
